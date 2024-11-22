7e57317c841e46c71f5d0499498e350b85e6989a
Evgeniy Moskalenko <evgeniy.moskalenko@gmail.com>
2024-11-21 19:06:11 -0800

    removed secret

--- classes/config.php
+++ classes/config.php
@@ -2,5 +2,4 @@
 
 class Config {
     const URL = 'https://api.openai.com/v1/chat/completions';
-    const API_KEY = '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';
 }
